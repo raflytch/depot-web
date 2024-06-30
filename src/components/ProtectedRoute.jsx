cd4537a0cdec627156c88a56d7ce42d70d435402
crlnravel <carleanoravel@gmail.com>
2024-06-30 23:10:41 +0700

    Update auth system

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,16 +1,21 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
+import Cookies from "js-cookie";
 
 const ProtectedRoute = ({ element, role }) => {
-  const { role: userRole } = useContext(AuthContext);
+  const { role: _role, token } = useContext(AuthContext);
 
-  if (!userRole) {
+  console.log(token);
+  console.log(_role);
+  console.log(role);
+
+  if (!_role) {
     // Jika role kosong, artinya pengguna belum login
     return <Navigate to="/login" />;
   }
 
-  if (role && role.toUpperCase() !== userRole.toUpperCase()) {
+  if (role.toUpperCase() !== _role.toUpperCase()) {
     // Jika role tidak sesuai dengan yang diharapkan
     return <Navigate to="/login" />;
   }
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,62 +1,65 @@
-import { createContext, useEffect, useReducer } from "react";
+import {createContext, useEffect, useReducer} from "react";
 import Cookies from "js-cookie";
-import { jwtDecode } from "jwt-decode";
+import {jwtDecode} from "jwt-decode";
 
 export const AuthContext = createContext(null);
 export const AuthDispatchContext = createContext(null);
 
-export function AuthProvider({ children }) {
-  const [state, dispatch] = useReducer(authReducer, initUser);
+export function AuthProvider({children}) {
+    const [state, dispatch] = useReducer(authReducer, initUser());
 
-  useEffect(() => {
-    const token = Cookies.get("access_token");
-
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        const userRole = decodedToken.role;
-
-        dispatch({
-          type: "logged-in",
-          user: {
-            nama: decodedToken.nama,
-            email: decodedToken.email,
-            role: userRole,
-            token: token,
-          },
-        });
-      } catch (err) {
-        // pass
-      }
-    }
-  }, []);
-
-  return (
-    <AuthContext.Provider value={state}>
-      <AuthDispatchContext.Provider value={dispatch}>
-        {children}
-      </AuthDispatchContext.Provider>
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={state}>
+            <AuthDispatchContext.Provider value={dispatch}>
+                {children}
+            </AuthDispatchContext.Provider>
+        </AuthContext.Provider>
+    );
 }
 
 function authReducer(state, action) {
-  switch (action.type) {
-    case "logged-in":
-      return action.user;
-    case "logged-out":
-      return {
+    switch (action.type) {
+        case "logged-in":
+            return action.user;
+        case "logged-out":
+            return {
+                nama: "",
+                email: "",
+                role: "",
+                token: "",
+            };
+    }
+}
+
+const initUser = () => {
+
+    const token = Cookies.get("access_token");
+
+    if (token) {
+        try {
+            const decodedToken = jwtDecode(token);
+
+            console.log(decodedToken)
+
+            return {
+                nama: decodedToken.nama,
+                email: decodedToken.email,
+                role: decodedToken.role,
+                token: token,
+            };
+        } catch (err) {
+            return {
+                nama: "",
+                email: "",
+                role: "",
+                token: "",
+            }
+        }
+    }
+    return {
         nama: "",
         email: "",
         role: "",
         token: "",
-      };
-  }
-}
-
-const initUser = {
-  nama: "",
-  email: "",
-  role: "",
-  token: "",
+    }
 };
