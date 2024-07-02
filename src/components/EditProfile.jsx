95f9c63e69728018c7728080d10ae08b77bc1e8a
raflytch <raflyazizabdillah30@gmail.com>
2024-07-02 21:37:58 +0700

    new changes

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,24 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Swal from "sweetalert2";
 import { BsPencilSquare } from "react-icons/bs";
+import { AuthContext, AuthDispatchContext } from "../contexts/AuthContext";
 
 const EditProfile = () => {
-  const [name, setName] = useState("");
-  const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const { nama: initialName, alamat: initialAddress } = useContext(AuthContext);
+  const dispatch = useContext(AuthDispatchContext);
+
+  const [name, setName] = useState(initialName);
+  const [address, setAddress] = useState(initialAddress);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (newPassword !== confirmPassword) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "New password and confirmation password do not match.",
-      });
-      return;
-    }
-
     // Handle the form submission logic here
     // For example, call an API to update the user's profile
 
@@ -29,23 +22,45 @@ const EditProfile = () => {
     });
   };
 
-  const renderInputWithIcon = (placeholder, value, onChange, id) => (
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const renderInputWithIcon = (placeholder, value, onChange, disabled) => (
     <div className="relative">
       <input
         type="text"
         value={value}
         onChange={onChange}
-        id={id}
-        className="w-full p-2 border border-gray-300 rounded"
+        className={`w-full p-2 border border-gray-300 rounded ${
+          disabled ? "bg-gray-100" : ""
+        }`}
         placeholder={placeholder}
+        disabled={disabled}
         required
       />
-      <BsPencilSquare className="absolute top-2 right-2 text-gray-500 cursor-pointer" />
+      {!disabled && (
+        <BsPencilSquare className="absolute top-2 right-2 text-gray-500 cursor-pointer" />
+      )}
     </div>
   );
 
   return (
     <div className="max-w-md mx-auto mt-10">
+      <div className="flex justify-center mb-4">
+        <div className="avatar online">
+          <div className="w-16 rounded-full">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              alt="Avatar"
+            />
+          </div>
+        </div>
+      </div>
       <h2 className="text-2xl font-bold mb-5">Edit Profile</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
@@ -55,44 +70,19 @@ const EditProfile = () => {
           {renderInputWithIcon(
             "Enter your name",
             name,
-            (e) => setName(e.target.value),
-            "name"
+            handleNameChange,
+            false
           )}
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="oldPassword">
-            Old Password
+          <label className="block text-sm font-bold mb-2" htmlFor="address">
+            Address
           </label>
           {renderInputWithIcon(
-            "Enter your old password",
-            oldPassword,
-            (e) => setOldPassword(e.target.value),
-            "oldPassword"
-          )}
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-bold mb-2" htmlFor="newPassword">
-            New Password
-          </label>
-          {renderInputWithIcon(
-            "Enter your new password",
-            newPassword,
-            (e) => setNewPassword(e.target.value),
-            "newPassword"
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            className="block text-sm font-bold mb-2"
-            htmlFor="confirmPassword"
-          >
-            Confirm New Password
-          </label>
-          {renderInputWithIcon(
-            "Confirm your new password",
-            confirmPassword,
-            (e) => setConfirmPassword(e.target.value),
-            "confirmPassword"
+            "Enter your address",
+            address,
+            handleAddressChange,
+            false
           )}
         </div>
         <button
