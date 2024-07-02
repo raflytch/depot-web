54e7ad9fdeff43a3c7f276bf724c4c1f95da547f
crlnravel <carleanoravel@gmail.com>
2024-07-02 20:57:10 +0700

    Add logout system

--- src/components/AvatarManageAccount.jsx
+++ src/components/AvatarManageAccount.jsx
@@ -1,28 +1,39 @@
-import React, { useContext } from "react";
-import { AuthDispatchContext, AuthContext } from "../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import React, {useContext} from "react";
+import {AuthDispatchContext, AuthContext} from "../contexts/AuthContext";
+import {useNavigate} from "react-router-dom";
 import Cookies from "js-cookie";
 
 const AvatarManageAccount = () => {
-  const dispatch = useContext(AuthDispatchContext);
-  const navigate = useNavigate();
+    const { token } = useContext(AuthContext);
+    const dispatch = useContext(AuthDispatchContext);
+    const navigate = useNavigate();
 
-  const handleLogout = () => {
-    Cookies.remove("access_token");
-    dispatch({ type: "logged-out" });
-    navigate("/");
-  };
+    const handleLogout = async () => {
+        const res = await fetch("http://localhost:3000/auth/logout", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
 
-  return (
-    <div className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-      <li>
-        <a href="/profile">Profil</a>
-      </li>
-      <li>
-        <a onClick={handleLogout}>Logout</a>
-      </li>
-    </div>
-  );
+        if (res.status !== 200) {
+            return alert("Logout gagal");
+        }
+
+        Cookies.remove("access_token");
+        dispatch({type: "logged-out"});
+        navigate("/");
+    };
+
+    return (
+        <div className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li>
+                <a href="/profile">Profil</a>
+            </li>
+            <li>
+                <a onClick={handleLogout}>Logout</a>
+            </li>
+        </div>
+    );
 };
 
 export default AvatarManageAccount;
