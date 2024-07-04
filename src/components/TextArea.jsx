711ebd74bc86fe3068c9c715f60f42fa51c6c7fa
raflytch <raflyazizabdillah30@gmail.com>
2024-07-05 02:35:40 +0700

    fix bug

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -11,7 +11,7 @@ const TextArea = ({ name, value, onChange, placeholder, required }) => {
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        className="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-none h-32 lg:h-40"
+        className="block p-2.5 w-full text-md text-gray-900 rounded-lg border border-gray-300 input resize-none h-32 lg:h-40"
         required={required}
       ></textarea>
     </div>
